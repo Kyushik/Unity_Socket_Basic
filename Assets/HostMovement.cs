7259af4ef41c44cdda21a18f1165a1a0ed675f18
Kyushik <kyushikmin@gmail.com>
2017-12-17 13:21:10 +0900

    First commit

---
+++ Assets/HostMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using SocketIO;
+
+public class HostMovement : MonoBehaviour {
+	public int action { get; set; }
+
+	// Use this for initialization
+	private IEnumerator Start() 
+	{
+		//DontDestroyOnLoad(this.gameObject);
+		yield return new WaitForSeconds(0.5f);
+		CommandServer.Instance.Init();
+		CommandServer.Instance.EmitTelemetry ();
+
+	}
+
+	// Update is called once per frame
+	private void Update() 
+	{
+		if (action == 1) {
+			print ("Connected!!");
+		}
+	}
+}
+
---
+++ Assets/Server_Script/CommandServer.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using SocketIO;
+using UnityStandardAssets.Vehicles.Car;
+using System;
+using System.Security.AccessControl;
+
+public class CommandServer : MonoBehaviour
+{
+
+	public static CommandServer _instance;
+	public static CommandServer Instance {
+		get{
+			if (!_instance) {
+				GameObject go = new GameObject ();
+				go.name = "CommandServer";
+				_instance = go.AddComponent (typeof(CommandServer)) as CommandServer;
+			}
+			return _instance;
+		}
+	}
+
+	public HostMovement HostMovement;
+//	public Camera FrontFacingCamera;
+//	public Camera RearFacingCamera;
+	private HostMovement _hostMovement;
+
+//	private String Front_camera_old;
+//	private String Rear_camera_old;
+//
+//	private String Front_cam_data;
+//	private String Rear_cam_data;
+
+	// Use this for initialization
+	void Start()
+	{
+		SocketIOComponent.Instance.On("open", OnOpen);
+		SocketIOComponent.Instance.On("onsteer", steer);
+		SocketIOComponent.Instance.On("manual", onManual);
+
+		Init ();
+
+		// If comment this, disconnection happens
+		DontDestroyOnLoad (this);
+	}
+
+	public void Init(){
+		
+		HostMovement = GameObject.FindWithTag ("Player").GetComponent<HostMovement> ();
+//		FrontFacingCamera = GameObject.Find("DriverViewCam").GetComponent<Camera> ();
+//		RearFacingCamera = GameObject.Find("BackwardViewCam").GetComponent<Camera> ();
+		_hostMovement = HostMovement.GetComponent<HostMovement>();
+
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+//		if (FrontFacingCamera && RearFacingCamera) {
+//			Front_cam_data = Convert.ToBase64String (CameraHelper.CaptureFrame (FrontFacingCamera));
+//			Rear_cam_data = Convert.ToBase64String (CameraHelper.CaptureFrame (RearFacingCamera));
+//		}
+	}
+
+	void OnOpen(SocketIOEvent obj)
+	{
+		Debug.Log("Connection Open");
+		//EmitTelemetry();
+	}
+
+	// 
+	void onManual(SocketIOEvent obj)
+	{
+		//EmitTelemetry ();
+	}
+//
+	void steer(SocketIOEvent obj)
+	{
+		JSONObject jsonObject = obj.data;
+		HostMovement.action = int.Parse (jsonObject.GetField ("action").str);
+
+		EmitTelemetry();
+	}
+
+	public void EmitTelemetry()
+	{
+		//UnityMainThreadDispatcher.Instance().Enqueue(() =>
+		//{
+			//print("Attempting to Send...");
+			// send only if it's not being manually driven
+			//if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.S))) {
+			//	_socket.Emit("telemetry", new JSONObject());
+			//}
+			//else {
+				// Collect Data from the Car
+				Dictionary<string, string> data = new Dictionary<string, string>();
+				//List<float> data_Lidar = new List<float>();
+				
+				data ["test"] = 1.ToString ("N4");
+//				data["Speed"] = _hostMovement.m_Speed.ToString ("N4");
+//				data["Action_vehicle"] = _hostMovement.action.ToString("N4");
+//				//data["reward"] = _hostMovement.reward.ToString("N4");
+//				data["Front_ADAS"] = _hostMovement.Front_ADAS.ToString("N4");
+//				data["Left_ADAS"] = _hostMovement.Left_ADAS.ToString("N4");
+//				data["Right_ADAS"] = _hostMovement.Right_ADAS.ToString("N4");
+//				data["Left_Changing"] = _hostMovement.Left_Changing.ToString("N4");
+//				data["Right_Changing"] = _hostMovement.Right_Changing.ToString("N4");
+//				data["Vehicle_X"] = _hostMovement.Vehicle_x.ToString("N4");
+//				data["Vehicle_Z"] = _hostMovement.Vehicle_z.ToString ("N4");
+
+				//data["terminal"] = _hostMovement.terminal.ToString("N4");
+
+//				if (HostMovement.Range_list.Count < 360) {
+//					for (int i = 0; i < 360; i++){
+//						data[i.ToString("D")] = 0.ToString("N4");
+//					}
+//				} else {
+//					for (int i = 0; i < 360; i++){
+//						data[i.ToString("D")] = HostMovement.Range_list[i].ToString("N4");
+//					}
+//				}
+					
+
+
+				//data_Lidar = HostMovement.Range_list.ToString("N4");
+				SocketIOComponent.Instance.Emit("telemetry", new JSONObject(data));
+				//_socket.Emit("telemetry", new JSONObject.Type.));
+			//}
+		//});
+
+	}
+
+	public void CloseSocket(){
+		SocketIOComponent.Instance.Close ();
+	}
+}
